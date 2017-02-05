61a9ad4cbb560c28a9d751c8de8cafd1c7cb43de
Sergey Polozov <fresher40@gmail.com>
2017-02-06 02:23:33 +0300

    information 1.2

--- ClientVkontakte/InformationViewController.swift
+++ ClientVkontakte/InformationViewController.swift
@@ -11,8 +11,7 @@ import Alamofire
 
 class InformationViewController: UIViewController, UITableViewDataSource {
     
-    var strings = [String]()
-
+    var information = [String]()
     @IBOutlet weak var tableView: UITableView!
     let informationList = ["privet", "kak dela", "u tebya"]
     
@@ -27,7 +26,7 @@ class InformationViewController: UIViewController, UITableViewDataSource {
         let keyChain = KeyChain()
         let token = keyChain.getToken()
         
-        getName(token: token)
+        getInformationAboutUser(token: token)
     }
     
     func loadFromDataBase() {
@@ -39,25 +38,44 @@ class InformationViewController: UIViewController, UITableViewDataSource {
         present(theTransition!, animated: true, completion: nil)
     }
     
-    func getName (token: String) {
+    func getInformationAboutUser (token: String) {
         Alamofire.request("https://api.vk.com/method/users.get?fields=bdate,sex,contacts,photo_max_orig&access_token=\(token)").responseJSON { response in
             if response.error != nil {
                 self.loadFromDataBase()
-            } else
-                if let result = response.result.value {
-                    let JSON = result as! NSDictionary
-                    if let arrayOfDictionary = JSON.value(forKey: "response") as? [NSDictionary]{
-                        let dictionary = arrayOfDictionary[0] as! NSDictionary
+            } else if let result = response.result.value {
+                let JSON = result as! NSDictionary
+                if let arrayOfDictionary = JSON.value(forKey: "response") as? [NSDictionary]{
+                    let dictionary = arrayOfDictionary[0] 
                         
-                        let name = dictionary.value(forKey: "first_name") as! String
-                        let secondName = dictionary.value(forKey: "last_name") as! String
-                        self.strings.append("\(name) \(secondName)")
-                        
-                        print(dictionary)
-                    } else {
-                        print("error of token")
-                        self.backToLoginViewController()
+                    let name = dictionary.value(forKey: "first_name") as! String
+                    let secondName = dictionary.value(forKey: "last_name") as! String
+                    self.information.append("\(name) \(secondName)")
+                    
+                    if let sex = dictionary.value(forKey: "sex") as! Int? {
+                        if sex == 2 {
+                            self.information.append("sex: man")
+                        } else {
+                            self.information.append("sex: woman")
+                        }
                     }
+                    
+                    if let bdate = dictionary.value(forKey: "bdate") as? String {
+                        self.information.append("birthday: \(bdate)")
+                    }
+                    
+                    if let phone = dictionary.value(forKey: "mobile_phone") as? String {
+                        self.information.append("phone: \(phone)")
+                    }
+                    
+                    if let homePhone = dictionary.value(forKey: "home_phone") as? String {
+                        self.information.append("home phone: \(homePhone)")
+                        print(self.information)
+                    }
+                    
+                } else {
+                    print("error of token")
+                    self.backToLoginViewController()
+                }
             }
         }
     }

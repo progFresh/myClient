904eb56bbcaadf3e762d49bdb05819b5a4a7cb97
Sergey Polozov <fresher40@gmail.com>
2017-02-06 04:52:34 +0300

    1.3

--- ClientVkontakte/InformationViewController.swift
+++ ClientVkontakte/InformationViewController.swift
@@ -9,11 +9,11 @@
 import UIKit
 import Alamofire
 
-class InformationViewController: UIViewController, UITableViewDataSource {
+class InformationViewController: UIViewController {
     
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var mainPhoto: UIImageView!
     var information = [String]()
-    @IBOutlet weak var tableView: UITableView!
-    let informationList = ["privet", "kak dela", "u tebya"]
     
     @IBAction func refreshButtonTapped(_ sender: Any) {
         viewDidLoad()
@@ -21,12 +21,13 @@ class InformationViewController: UIViewController, UITableViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.dataSource = self
+        information.removeAll()
         
+        setConstraints()
         let keyChain = KeyChain()
         let token = keyChain.getToken()
         
-        getInformationAboutUser(token: token)
+        fillInformationAboutUser(token: token)
     }
     
     func loadFromDataBase() {
@@ -38,19 +39,18 @@ class InformationViewController: UIViewController, UITableViewDataSource {
         present(theTransition!, animated: true, completion: nil)
     }
     
-    func getInformationAboutUser (token: String) {
-        Alamofire.request("https://api.vk.com/method/users.get?fields=bdate,sex,contacts,photo_max_orig&access_token=\(token)").responseJSON { response in
+    func fillInformationAboutUser (token: String) {
+        Alamofire.request("https://api.vk.com/method/users.get?fields=bdate,sex,contacts,photo_max,connections&access_token=\(token)").responseJSON { response in
             if response.error != nil {
                 self.loadFromDataBase()
             } else if let result = response.result.value {
                 let JSON = result as! NSDictionary
                 if let arrayOfDictionary = JSON.value(forKey: "response") as? [NSDictionary]{
-                    let dictionary = arrayOfDictionary[0] 
-                        
+                    
+                    let dictionary = arrayOfDictionary[0]
                     let name = dictionary.value(forKey: "first_name") as! String
                     let secondName = dictionary.value(forKey: "last_name") as! String
                     self.information.append("\(name) \(secondName)")
-                    
                     if let sex = dictionary.value(forKey: "sex") as! Int? {
                         if sex == 2 {
                             self.information.append("sex: man")
@@ -58,20 +58,22 @@ class InformationViewController: UIViewController, UITableViewDataSource {
                             self.information.append("sex: woman")
                         }
                     }
-                    
                     if let bdate = dictionary.value(forKey: "bdate") as? String {
                         self.information.append("birthday: \(bdate)")
                     }
-                    
                     if let phone = dictionary.value(forKey: "mobile_phone") as? String {
                         self.information.append("phone: \(phone)")
                     }
-                    
                     if let homePhone = dictionary.value(forKey: "home_phone") as? String {
                         self.information.append("home phone: \(homePhone)")
-                        print(self.information)
                     }
-                    
+                    if let skype = dictionary.value(forKey: "skype") as? String {
+                        self.information.append("skype: \(skype)")
+                    }
+                    if let instagram = dictionary.value(forKey: "instagram") as? String {
+                        self.information.append("instagram: \(instagram)")
+                    }
+                    print(self.information)
                 } else {
                     print("error of token")
                     self.backToLoginViewController()
@@ -80,15 +82,28 @@ class InformationViewController: UIViewController, UITableViewDataSource {
         }
     }
     
-    // Mark : TableView
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return informationList.count
-    }
-    
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let tableCell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath)
-        tableCell.textLabel?.text = informationList[indexPath.row]
-        return tableCell
+    func setConstraints () {
+        let mainView = self.view
+        let screenSize = UIScreen.main.bounds
+        self.mainPhoto.translatesAutoresizingMaskIntoConstraints = false
+        //mainPhoto = 0 from left edge of screen
+        let leadingConstraint = NSLayoutConstraint(item: mainPhoto, attribute: .leading, relatedBy: .equal, toItem: mainView, attribute: .leading, multiplier: 1.0, constant: 0)
+        //mainPhoto = 63 from top of screen
+        let topConstraint = NSLayoutConstraint(item: mainPhoto, attribute: .top, relatedBy: .equal, toItem: mainView, attribute: .top, multiplier: 1.0, constant: 64)
+        //mainPhoto width
+        let mainPhotoWidth = NSLayoutConstraint(item: mainPhoto, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: (screenSize.width/2))
+        //mainPhoto height
+        let mainPhotoHeight = NSLayoutConstraint(item: mainPhoto, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: (screenSize.width/2))
+        self.view.addConstraints([leadingConstraint, topConstraint, mainPhotoWidth, mainPhotoHeight])
+        
+        self.infoLabel.translatesAutoresizingMaskIntoConstraints = false
+        //Label = 5 from left edge of photo
+        let leadingConstraintLabel = NSLayoutConstraint(item: infoLabel, attribute: .leading, relatedBy: .equal, toItem: mainPhoto, attribute: .trailing, multiplier: 1.0, constant: 5)
+        //Label = 63 from top of superView
+        let topConstraintLabel = NSLayoutConstraint(item: infoLabel, attribute: .top, relatedBy: .equal, toItem: mainView, attribute: .top, multiplier: 1.0, constant: 64)
+        //Label width
+        let WidthLabel = NSLayoutConstraint(item: infoLabel, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: (screenSize.width/2))
+        self.view.addConstraints([leadingConstraintLabel, topConstraintLabel, WidthLabel])
     }
 
 }
